4132de86711b145d87656d2deb1c99a92ac3cc92
econaz <econazx@gmail.com>
2026-05-16 21:02:35 +0800

    add dart

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -10,9 +10,30 @@
 ;;     ;; Optional packages
 ;;     lsp-ui))
 
+(use-package dart-mode
+  :defines (projectile-project-root-files-bottom-up)
+  :config
+  (with-eval-after-load "projectile"
+    (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+    (add-to-list 'projectile-project-root-files-bottom-up "BUILD")))
+
+(reformatter-define dart-format
+  :program "dart"
+  :args '("format"))
+
+(with-eval-after-load "dart-mode"
+  (define-key dart-mode-map (kbd "C-c C-o") 'dart-format-buffer))
+
+(use-package flutter
+  :ensure t
+  :after dart-mode
+  :custom
+  (flutter-sdk-path "~/Flutter/flutter/"))
 
 (use-package lsp-dart
   :ensure t
+  :init
+  (setq lsp-dart-sdk-dir "~/Flutter/flutter/bin/cache/dart-sdk/")
   :hook (dart-mode . lsp)
   :custom
   (lsp-dart-complete-function-calls t)
